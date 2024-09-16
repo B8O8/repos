addadffced8eaea4f6307fa01a04e7d741216252
B8O8 <charbel_makhlouf76@live.com>
2024-09-16 21:07:44 +0300

    fix backend url

--- front/src/utils/constants.tsx
+++ front/src/utils/constants.tsx
@@ -1,4 +1,4 @@
 export const defaultPassword = "123123";
-export const API_BASE_URL = "http://localhost:5000";
-// export const API_BASE_URL = "https://backend.masari.me"
+// export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://backend.masari.me"
 export const CALCULATED_WIDTH = "calc(90vw - 10vw - 20px)";
